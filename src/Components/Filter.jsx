e245eb9c62dc3efaf909a31a79d92326ae0fee8f
amirsuhaill <amirasuhail01@gmail.com>
2024-07-13 15:50:07 +0530

    newCommit

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -9,8 +9,8 @@ function Filter({ filterData,category,setCategory }) {
     return(
         <div className='flex flex-col md:flex-row gap-4 justify-center text-white my-2 items-center'>
             {filterData.map((data) => (
-                <button key={data.id} type="button" onClick={() => filterHandler(data.title)} className={`bg-indigo-900 py-1 px-3 text-xl max-w-[250px] rounded-md hover:bg-indigo-600 duration-300
-                ${category === data.title ?  "border-2 border-white" : "border-none"}
+                <button key={data.id} type="button" onClick={() => filterHandler(data.title)} className={`bg-indigo-900 py-1 px-3 text-xl max-w-[250px] rounded-md  duration-300
+                ${category === data.title ?  "border-2 border-white bg-indigo-600" : "border-none"}
                 `}>{data.title}</button>
             ))}
         </div> 
